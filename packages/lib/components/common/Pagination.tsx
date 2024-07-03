60ce211a361c21a615605918a8d37bbe43198bb9
Karlen9 <misakyankarlen@gmail.com>
2024-07-03 21:34:04 +0400

    feat: pagination styles

--- packages/lib/components/common/Pagination.tsx
+++ packages/lib/components/common/Pagination.tsx
@@ -29,8 +29,8 @@ export const Pagintaion = (props: TPaginationProps): ReactElement => {
 	};
 
 	return (
-		<div className={'flex w-full  pt-6'}>
-			<div className={'flex-start whitespace-nowrap md:absolute '}>
+		<div className={'flex w-full pt-6'}>
+			<div className={'flex-start mt-0 whitespace-nowrap md:absolute md:mt-3 '}>
 				{'Showing '}
 				{getFrom()}
 				{' to '}
@@ -44,19 +44,27 @@ export const Pagintaion = (props: TPaginationProps): ReactElement => {
 					className={'flex items-center gap-x-4'}
 					pageCount={amountOfPages}
 					previousLabel={
-						<button onClick={prevPage}>
-							<IconArrowLeft className={'size-5'} />
+						<button
+							onClick={prevPage}
+							className={'hover:bg-gray-0 mb-2 rounded-lg p-3 outline-1 outline-gray-100 hover:outline'}>
+							<IconArrowLeft className={'size-3'} />
 						</button>
 					}
 					nextLabel={
-						<button onClick={nextPage}>
-							<IconArrowLeft className={'size-5 rotate-180 text-white'} />
+						<button
+							onClick={nextPage}
+							className={
+								'hover:bg-gray-0 mb-2 rounded-lg p-3 text-white outline-1 outline-gray-100 hover:outline'
+							}>
+							<IconArrowLeft className={'size-3 rotate-180 text-white'} />
 						</button>
 					}
 					onPageChange={({selected}) => onPageClick(selected + 1)}
 					pageRangeDisplayed={1}
 					marginPagesDisplayed={5}
-					pageClassName={'text-gray-100'}
+					pageClassName={
+						'text-gray-100 hover:outline outline-gray-100 outline-1 rounded-lg px-4 py-2 hover:text-white hover:bg-gray-0'
+					}
 					activeClassName={'text-white'}
 					breakClassName={'text-gray-100'}
 					nextClassName={'mt-2'}
--- packages/lib/components/icons/IconArrowLeft.tsx
+++ packages/lib/components/icons/IconArrowLeft.tsx
@@ -6,14 +6,14 @@ function IconArrowLeft(props: React.SVGProps<SVGSVGElement>): ReactElement {
 	return (
 		<svg
 			{...props}
-			width={'16'}
-			height={'16'}
-			viewBox={'0 0 16 16'}
+			width={'10'}
+			height={'8'}
+			viewBox={'0 0 10 8'}
 			fill={'none'}
 			xmlns={'http://www.w3.org/2000/svg'}>
 			<path
-				d={'M12 7.66667L4 7.66667M4 7.66667L6.66667 5M4 7.66667L6.66667 10.3333'}
-				stroke={'currentcolor'}
+				d={'M9 3.66667L1 3.66667M1 3.66667L3.66667 1M1 3.66667L3.66667 6.33333'}
+				stroke={'currentColor'}
 				strokeOpacity={'0.3'}
 				strokeWidth={'1.5'}
 				strokeLinecap={'round'}
