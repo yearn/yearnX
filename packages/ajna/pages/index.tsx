89e732fee023371f5438ca0cdc66cfaa341ba868
Majorfi <major@ycorpo.com>
2024-07-31 11:46:06 +0200

    feat: add other spaces

---
+++ packages/ajna/pages/index.tsx
@@ -0,0 +1,46 @@
+import {type ReactElement} from 'react';
+import {DefaultHeader} from '@lib/components/common/DefaultHeader';
+import {Footer} from '@lib/components/common/Footer';
+import {VaultList} from '@lib/components/common/VaultList';
+import {useFetchYearnVaults} from '@lib/hooks/useYearnVaults';
+import {Section} from '@lib/sections';
+import {useDeepCompareMemo} from '@react-hookz/web';
+
+import {HEADER_TABS, VARIANT_TO_USE, VAULT_FILTER} from '../constants';
+
+export default function Index(): ReactElement {
+	const {vaults, isLoading} = useFetchYearnVaults(VAULT_FILTER);
+	const vaultsValues = useDeepCompareMemo(() => Object.values(vaults), [vaults]);
+
+	return (
+		<section className={'flex w-full max-w-[1200px] flex-col gap-y-6'}>
+			<DefaultHeader
+				docsLink={''}
+				secondLogoURL={'/partnerLogo.svg'}
+			/>
+			<Section
+				variant={VARIANT_TO_USE}
+				bgImage={'/bg.webp'}
+				title={'YEARN PARTNER VAULTS'}
+				description={
+					'Several lines description. Several lines description. Several lines description. Several lines description. '
+				}
+				cards={[
+					{title: 'Grand prize', currency: 'ETH', value: 192, decimals: 2, isReady: true},
+					{title: 'Grand prize', currency: 'ETH', value: 111, decimals: 2, isReady: true},
+					{title: 'Grand prize', currency: 'ETH', value: 1, decimals: 2, isReady: true}
+				]}
+			/>
+			<VaultList
+				vaults={vaultsValues}
+				isLoading={isLoading}
+				headerTabs={HEADER_TABS}
+			/>
+
+			<Footer
+				docsLink={''}
+				secondLogoURL={''}
+			/>
+		</section>
+	);
+}
---
+++ packages/velodrome/tailwind.config.ts
@@ -0,0 +1,26 @@
+const {COLORS} = require('./constants');
+
+/** @type {import('tailwindcss').Config} */
+const config = require('../lib/tailwind.config');
+
+module.exports = {
+	...config,
+	content: [
+		...config.content,
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./contexts/**/*.{js,ts,jsx,tsx}',
+		'./hooks/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}'
+	],
+	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('tailwindcss-animate')],
+	theme: {
+		...config.theme,
+		extend: {
+			...config.theme.extend,
+			colors: {
+				...config.theme.extend.colors,
+				...COLORS
+			}
+		}
+	}
+};
