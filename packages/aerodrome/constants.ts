912003a803221c08383f50de5c0306e8900ccfda
Majorfi <major@ycorpo.com>
2024-08-05 10:56:17 +0200

    fix: aerocolors

--- packages/aerodrome/constants.ts
+++ packages/aerodrome/constants.ts
@@ -13,13 +13,13 @@ export const HEADER_TABS = [
 ];
 
 export const COLORS = {
-	button: '#ff0420', // The main color of the button
-	buttonHover: '#b80018', // The hover color of the button
-	regularText: '#000000', // The color of the regular text
+	button: '#0052FF', // The main color of the button
+	buttonHover: '#054DED', // The hover color of the button
+	regularText: '#FFFFFF', // The color of the regular text
 	accentText: '#FFFFFF', // The color of the text on the button or the primary color
-	background: '#fff0f1', // The background color of the page
-	table: '#FFFFFF', // The background color of the table (front element)
-	primary: '#ff0420', // The primary color of the page
-	secondary: '#3374db', // The secondary color of the page
-	headerTag: '#ff0420' // The color of the header tag [Yearn x Something]
+	background: '#151B33', // The background color of the page
+	table: '#1E233D', // The background color of the table (front element)
+	primary: '#0052FF', // The primary color of the page
+	secondary: '#FF1001', // The secondary color of the page
+	headerTag: '#232947' // The color of the header tag [Yearn x Something]
 };
