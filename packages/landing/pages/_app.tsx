baa0e461fe1e2389039e3badc5b466247afb0961
Majorfi <major@ycorpo.com>
2024-08-06 11:10:55 +0200

    feat: add plausible

--- packages/landing/pages/_app.tsx
+++ packages/landing/pages/_app.tsx
@@ -1,3 +1,4 @@
+import PlausibleProvider from 'next-plausible';
 import {Meta} from '@lib/components/common/Meta';
 import {WithFonts} from '@lib/components/common/WithFonts';
 
@@ -11,21 +12,26 @@ import '@lib/style.css';
 function MyApp(props: AppProps): ReactElement {
 	return (
 		<WithFonts>
-			<Meta
-				title={'Yearn Space'}
-				titleColor={'#FFFFFF'}
-				themeColor={'#000000'}
-				description={
-					'Ape Stronger Together. Yearn partners with the best in the industry to bring you the best yield farming strategies.'
-				}
-				og={'https://yearn.space/og.png'}
-				uri={'https://yearn.space'}
-			/>
-			<div className={'bg-background flex w-full flex-col justify-center '}>
-				<main className={'relative mb-0  flex min-h-screen w-full flex-col pb-8 md:pb-0'}>
-					<Layout {...props} />
-				</main>
-			</div>
+			<PlausibleProvider
+				domain={process.env.PLAUSIBLE_DOMAIN || 'yearn.space'}
+				pageviewProps={{title: 'Yearn Space'}}
+				enabled={true}>
+				<Meta
+					title={'Yearn Space'}
+					titleColor={'#FFFFFF'}
+					themeColor={'#000000'}
+					description={
+						'Ape Stronger Together. Yearn partners with the best in the industry to bring you the best yield farming strategies.'
+					}
+					og={'https://yearn.space/og.png'}
+					uri={'https://yearn.space'}
+				/>
+				<div className={'bg-background flex w-full flex-col justify-center '}>
+					<main className={'relative mb-0  flex min-h-screen w-full flex-col pb-8 md:pb-0'}>
+						<Layout {...props} />
+					</main>
+				</div>
+			</PlausibleProvider>
 		</WithFonts>
 	);
 }
--- packages/optimism/pages/_app.tsx
+++ packages/optimism/pages/_app.tsx
@@ -1,4 +1,5 @@
 import {Toaster} from 'react-hot-toast';
+import PlausibleProvider from 'next-plausible';
 import {WalletContextApp} from '@builtbymom/web3/contexts/useWallet';
 import {WithMom} from '@builtbymom/web3/contexts/WithMom';
 import {Meta} from '@lib/components/common/Meta';
@@ -22,54 +23,62 @@ export default function MyApp(props: AppProps): ReactElement {
 	const {Component} = props;
 	return (
 		<WithFonts>
-			<Meta
-				title={PROJECT_TITLE}
-				description={PROJECT_DESCRIPTION}
-				titleColor={COLORS.background}
-				themeColor={COLORS.primary}
-				og={''}
-				uri={''}
-			/>
-			<WithMom
-				supportedChains={supportedNetworks}
-				tokenLists={['https://raw.githubusercontent.com/SmolDapp/tokenLists/main/lists/yearn-min.json']}>
-				<WalletContextApp>
-					<WithPopularTokens>
-						<WithPrices supportedNetworks={supportedNetworks}>
-							<VaultsContextApp>
-								<SolverContextApp>
-									<div className={'bg-background flex h-lvh w-full justify-center overflow-auto p-6'}>
-										<main className={'relative mb-auto flex w-full justify-center'}>
-											<Component />
-										</main>
-									</div>
-								</SolverContextApp>
-							</VaultsContextApp>
-						</WithPrices>
-					</WithPopularTokens>
-				</WalletContextApp>
-			</WithMom>
-			<Toaster
-				toastOptions={{
-					duration: 5000,
-					className: 'toast',
-					success: {
-						icon: <IconCheck className={'-mr-1 size-5 min-h-5 min-w-5 pt-1.5'} />,
-						iconTheme: {
-							primary: 'black',
-							secondary: '#F1EBD9'
+			<PlausibleProvider
+				domain={process.env.PLAUSIBLE_DOMAIN || 'yearn.space'}
+				pageviewProps={{title: PROJECT_TITLE}}
+				enabled={true}>
+				<Meta
+					title={PROJECT_TITLE}
+					description={PROJECT_DESCRIPTION}
+					titleColor={COLORS.background}
+					themeColor={COLORS.primary}
+					og={''}
+					uri={''}
+				/>
+				<WithMom
+					supportedChains={supportedNetworks}
+					tokenLists={['https://raw.githubusercontent.com/SmolDapp/tokenLists/main/lists/yearn-min.json']}>
+					<WalletContextApp>
+						<WithPopularTokens>
+							<WithPrices supportedNetworks={supportedNetworks}>
+								<VaultsContextApp>
+									<SolverContextApp>
+										<div
+											className={
+												'bg-background flex h-lvh w-full justify-center overflow-auto p-6'
+											}>
+											<main className={'relative mb-auto flex w-full justify-center'}>
+												<Component />
+											</main>
+										</div>
+									</SolverContextApp>
+								</VaultsContextApp>
+							</WithPrices>
+						</WithPopularTokens>
+					</WalletContextApp>
+				</WithMom>
+				<Toaster
+					toastOptions={{
+						duration: 5000,
+						className: 'toast',
+						success: {
+							icon: <IconCheck className={'-mr-1 size-5 min-h-5 min-w-5 pt-1.5'} />,
+							iconTheme: {
+								primary: 'black',
+								secondary: '#F1EBD9'
+							}
+						},
+						error: {
+							icon: <IconCircleCross className={'-mr-1 size-5 min-h-5 min-w-5 pt-1.5'} />,
+							iconTheme: {
+								primary: 'black',
+								secondary: '#F1EBD9'
+							}
 						}
-					},
-					error: {
-						icon: <IconCircleCross className={'-mr-1 size-5 min-h-5 min-w-5 pt-1.5'} />,
-						iconTheme: {
-							primary: 'black',
-							secondary: '#F1EBD9'
-						}
-					}
-				}}
-				position={'top-right'}
-			/>
+					}}
+					position={'top-right'}
+				/>
+			</PlausibleProvider>
 		</WithFonts>
 	);
 }
